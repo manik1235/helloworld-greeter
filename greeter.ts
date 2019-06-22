4098f2163dfa1b8083ac8559af847c5cea228572
Andrew Henning-Kolberg <andrew.kolberg@gmail.com>
2019-06-22 16:34:28 -0400

    init commit. simple greeter example.

---
+++ greeter.ts
@@ -0,0 +1,21 @@
+class Student {
+  fullName: string;
+  constructor(public firstName: string, public middleInitial: string, public lastName: string) {
+    this.fullName = firstName + " " + middleInitial + " " + lastName;
+  }
+}
+
+interface Person {
+  firstName: string;
+  middleInitial: string;
+  lastName: string;
+  fullName: string;
+}
+
+function greeter (person: Person) {
+  return "Hello, " + person.fullName;
+}
+
+let user = new Student("Jane", "M.", "User");
+
+document.body.innerHTML = greeter(user);
